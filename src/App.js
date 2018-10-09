04f918e3627c26dc399dee25f100c3164ebad248
Anaele Salvatore <anaele.salvatore@gmail.com>
2018-10-09 09:59:54 +0200

    modification liens

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
-import { Route, BrowserRouter, Switch } from 'react-router-dom';
+import { Route, BrowserRouter, Switch, NavLink } from 'react-router-dom';
 import Home from './Home';
-import History from './History';
+import History from './notre-histoire';
+import "./App.css"
 
 
 class App extends Component {
     render() {
         return (
             <BrowserRouter>
+            <div>
+            <NavLink to="/" activeClassName="selected"> Accueil </NavLink>
+            <NavLink to="/notre-histoire" activeClassName="selected"> Notre histoire </NavLink>
               <Switch>
-                <Route exact path="/" component={Home} />
+                <Route exact path="/" component={Home}/>
                 <Route path="/notre-histoire" component={History} /> 
               </Switch>
+            </div>
             </BrowserRouter>
         );
     }
--- src/notre-histoire.js
+++ src/notre-histoire.js
@@ -1,20 +1,17 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 
-class Home extends Component {
+class History extends Component {
     render(){
         return(
             <div>
-            <Link to="/"> Retour à l'accueil </Link>
+            <Link to="/"> Retour accueil. </Link>
               {this.props.match.params.productNumber}
-            <h1>Bienvenue sur l'accueil.</h1>
+            <h1>Je vais vous racontez notre histoire. Il était une fois, à la Wild Code School...</h1>
             </div>
         )
     }
 }
 
 
-export default Home;
-
-
-
+export default History;
